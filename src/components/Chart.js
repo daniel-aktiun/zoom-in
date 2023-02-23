55ddd0a361649cc2a3d6b69499c36d83d976da8e
daniel-aktiun <daniel@aktiun.com>
2023-02-23 13:04:49 -0500

    feat: add buttons

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -27,6 +27,34 @@ const getOptions = (fullScreen = false) => ({
       data: [randomNumber(), randomNumber(), randomNumber()],
     },
   ],
+  exporting: {
+    // menuItemDefinitions: {
+    //   // Custom definition
+    //   label: {
+    //     onclick: function () {
+    //       this.renderer
+    //         .label("You just clicked a custom menu item", 100, 100)
+    //         .attr({
+    //           fill: "#a4edba",
+    //           r: 5,
+    //           padding: 10,
+    //           zIndex: 10,
+    //         })
+    //         .css({
+    //           fontSize: "1.5em",
+    //         })
+    //         .add();
+    //     },
+    //     text: "Show label",
+    //   },
+    // },
+    buttons: {
+      contextButton: {
+        // menuItems: ["downloadPNG", "downloadSVG", "separator", "label"],
+        menuItems: ["downloadPNG", "separator", "downloadSVG"],
+      },
+    },
+  },
   credits: false,
 });
 
